6da0ad76f5e179d4a264af6b859e8788cfd44950
leolambo <leoyambo@yahoo.com>
2016-10-14 13:34:38 -0400

    Intialized Queue; Skelton for onClick methods"

--- app/src/main/java/leo_santi/heads_up/MainActivity.java
+++ app/src/main/java/leo_santi/heads_up/MainActivity.java
@@ -3,11 +3,26 @@ package leo_santi.heads_up;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MainActivity extends AppCompatActivity {
+    private Queue<String> words;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        words = new LinkedList<String>(Arrays.asList("cat","dog","cow","Donald Trump","","","","","","","","",""));
+    }
+
+    private void playerCorrect(){
+
+    }
+
+    private void playerWrong(){
+
     }
 }
