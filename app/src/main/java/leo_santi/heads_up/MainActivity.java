9226bc0215fe7ed7cc8b5bd21ef620da137cd958
leolambo <leoyambo@yahoo.com>
2016-10-14 14:05:19 -0400

    Implemented playerCorrect Method

--- app/src/main/java/leo_santi/heads_up/MainActivity.java
+++ app/src/main/java/leo_santi/heads_up/MainActivity.java
@@ -2,6 +2,7 @@ package leo_santi.heads_up;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -9,20 +10,35 @@ import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
     private Queue<String> words;
-
+    private int playerOneScore;
+    private int playerTwoScore;
+    private boolean playerOneTurn = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        words = new LinkedList<String>(Arrays.asList("cat","dog","cow","Donald Trump","","","","","","","","",""));
+        words = new LinkedList<String>(Arrays.asList("cat","dog","cow","Donald Trump",
+                "","","","","","","","",""));
+        playerOneScore = 0;
+        playerTwoScore = 0;
     }
 
     private void playerCorrect(){
+        TextView label = (TextView) findViewById(R.id.correctScore);
+        Integer score = playerOneTurn ? playerOneScore : playerTwoScore;
 
+        score++;
+        label.setText(score);
+
+
+        if (playerOneTurn)
+            playerOneScore = score;
+        else
+            playerTwoScore = score;
     }
 
-    private void playerWrong(){
+    private void playerSkip(){
 
     }
 }
